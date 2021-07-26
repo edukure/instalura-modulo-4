594ac0dd748b0f6cd5a399a236999e344ae7b3bc
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 12:52:46 -0300

    Links

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -2,11 +2,38 @@ import { Logo } from '../../../theme/Logo';
 import { MenuWrapper } from './styles';
 
 const Menu = () => {
+  const links = [
+    {
+      texto: 'Home',
+      url: '/',
+    },
+    {
+      texto: 'Perguntas frequentes',
+      url: '/faq',
+    },
+    {
+      texto: 'Sobre',
+      url: '/sobre',
+    },
+  ];
+
   return (
     <MenuWrapper>
-      <MenuWrapper.LeftSide><Logo /></MenuWrapper.LeftSide>
-      <MenuWrapper.CentralSide>Centro</MenuWrapper.CentralSide>
-      <MenuWrapper.RightSide>Direita</MenuWrapper.RightSide>
+      <MenuWrapper.LeftSide>
+        <Logo />
+      </MenuWrapper.LeftSide>
+      <MenuWrapper.CentralSide>
+        {links.map((link) => {
+          return (
+            <li>
+              <a href={link.url}>{link.texto}</a>
+            </li>
+          );
+        })}
+      </MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide>
+        <button>Entrar</button>
+      </MenuWrapper.RightSide>
     </MenuWrapper>
   );
 };
