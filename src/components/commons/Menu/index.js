034b802a831ef6cdaad3a234f542bce2c73ed81d
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 12:50:41 -0300

    Logo

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,9 +1,10 @@
+import { Logo } from '../../../theme/Logo';
 import { MenuWrapper } from './styles';
 
 const Menu = () => {
   return (
     <MenuWrapper>
-      <MenuWrapper.LeftSide>Esquerda</MenuWrapper.LeftSide>
+      <MenuWrapper.LeftSide><Logo /></MenuWrapper.LeftSide>
       <MenuWrapper.CentralSide>Centro</MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>Direita</MenuWrapper.RightSide>
     </MenuWrapper>
