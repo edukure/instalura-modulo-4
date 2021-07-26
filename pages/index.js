3fcf486eed310b308aa86128bd7a031e57dfe85a
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 13:37:03 -0300

    Criar Footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        flex: '1',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
       <Menu />
+      <Footer />
     </div>
   );
 }
