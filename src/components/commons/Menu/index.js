ac64fa86777dc0c93775916c073e6a795f1c7112
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 12:47:10 -0300

    Menu básico

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,13 @@
+import { MenuWrapper } from './styles';
+
+const Menu = () => {
+  return (
+    <MenuWrapper>
+      <div>Esquerda</div>
+      <div>Centro</div>
+      <div>Direita</div>
+    </MenuWrapper>
+  );
+};
+
+export default Menu;
---
+++ src/components/commons/Menu/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MenuWrapper = styled.nav`
+  font-family: 'Rubik', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 18px;
+  padding-left: 28px;
+  padding-right: 28px;
+`;
