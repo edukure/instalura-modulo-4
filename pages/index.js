96ce8ebda1d905f57414bfabfcf20465511ce534
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 19:29:48 -0300

    Adicionar conteúdo ao Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { Button } from '../src/components/commons/Button';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
+import Text from '../src/components/foundation/Text';
 
 export default function Home() {
   return (
@@ -12,6 +14,20 @@ export default function Home() {
         justifyContent: 'space-between',
       }}>
       <Menu />
+
+      <div>
+        <Text variant="title" tag="h1">
+          Compartilhe momentos e conecte-se com amigos
+        </Text>
+        
+        <Text variant="paragraph1" tag="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          industrys standard dummy text ever since the 1500s.
+        </Text>
+
+        <Button variant="primary.main">Cadastrar</Button>
+      </div>
+
       <Footer />
     </div>
   );
