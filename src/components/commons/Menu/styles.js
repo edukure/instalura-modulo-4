c3dbc6a6571fbda82f04d8c5e5fa11ac2d5d7b5f
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-26 19:23:31 -0300

    Implementar breakpointsMedia no Menu

--- src/components/commons/Menu/styles.js
+++ src/components/commons/Menu/styles.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { TextStyleVariantsMap } from '../../foundation/Text';
 
 export const MenuWrapper = styled.nav`
   font-family: 'Rubik', sans-serif;
@@ -9,12 +11,39 @@ export const MenuWrapper = styled.nav`
   margin-top: 18px;
   padding-left: 28px;
   padding-right: 28px;
+
+  ${breakpointsMedia({
+    md: css`
+      justify-content: flex-start;
+      margin-top: 32px;
+      margin-left: auto;
+      margin-right: auto;
+      width: 100%;
+      padding: 0 16px;
+      max-width: 768px;
+    `,
+    lg: css`
+      max-width: 1160px;
+    `,
+    xl: css`
+      max-width: 1222px;
+    `,
+  })}
 `;
 
 MenuWrapper.LeftSide = styled.div`
   padding: 0;
   margin: 0;
   order: 1;
+
+  ${breakpointsMedia({
+    md: css`
+      width: 131px;
+      height: 32px;
+      order: initial;
+      padding-right: 16px;
+    `,
+  })}
 `;
 
 MenuWrapper.CentralSide = styled.div`
@@ -31,12 +60,33 @@ MenuWrapper.CentralSide = styled.div`
   border-bottom: 1px solid #88989e;
   padding: 12px;
 
+  ${breakpointsMedia({
+    md: css`
+      max-width: 332px;
+      justify-content: space-between;
+      flex: 1;
+      order: initial;
+      border: none;
+      margin: 0;
+      padding-top: 0;
+      padding-bottom: 0;
+    `,
+  })}
+
   a {
     text-align: center;
     display: block;
     text-decoration: none;
     color: #88989e;
     transition: 200ms ease-in-out;
+    ${breakpointsMedia({
+      xs: css`
+        ${TextStyleVariantsMap.smallestException}
+      `,
+      md: css`
+        ${TextStyleVariantsMap.paragraph1}
+      `,
+    })}
     &:hover,
     &:focus {
       font-weight: 500;
@@ -52,4 +102,10 @@ MenuWrapper.RightSide = styled.div`
   flex: 1;
   order: 2;
   justify-content: flex-end;
+
+  ${breakpointsMedia({
+    md: css`
+      order: initial;
+    `,
+  })}
 `;
