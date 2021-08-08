6b975a5da27fd095b66db0e3d0b80837029ddbae
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-08 17:57:57 -0300

    Adicionar React.useState e handleChange dos inputs

--- src/components/patterns/FormCadastro/index.js
+++ src/components/patterns/FormCadastro/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 function FormContent() {
+  const [userInfo, setUserInfo] = React.useState({
+    usuario: 'omariosouto',
+    email: 'devsoutinho@gmail.com',
+  });
+
+  function handleChange(event) {
+    const fieldName = event.target.getAttribute('name');
+
+    setUserInfo({
+      ...userInfo,
+      [fieldName]: event.target.value,
+    });
+  }
+
   return (
     <form>
       <div>
-        <input placeholder="Email" name="email" value="devsoutinho@gmail.com" />
+        <input placeholder="Email" name="email" value={userInfo.email} onChange={handleChange} />
       </div>
       <div>
-        <input placeholder="Usuário" name="usuario" value="omariosouto" />
+        <input
+          placeholder="Usuário"
+          name="usuario"
+          value={userInfo.usuario}
+          onChange={handleChange}
+        />
       </div>
 
       <button type="submit">Cadastrar</button>
