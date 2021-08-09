357557980dfdc82ddc959a626a33312713721dee
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-09 17:56:11 -0300

    Mudar linebreak-style para unix no eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
   },
 };
