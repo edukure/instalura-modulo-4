1103af7e9da66f27a08e06f33ec32eed1ecc51cb
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-26 17:35:39 -0300

    implementar getStaticProps no FAQPage

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQPage() {
-  const [faqCategories, setFaqCategories] = React.useState([]);
+// eslint-disable-next-line react/prop-types
+export default function FAQPage({ faqCategories }) {
+  // const [faqCategories, setFaqCategories] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch('https://instalura-api.vercel.app/api/content/faq')
-      .then((respostaDoServer) => respostaDoServer.json())
-      .then((respostaConvertida) => respostaConvertida.data)
-      .then((resposta) => {
-        setFaqCategories(resposta);
-      });
-  }, []);
+  // React.useEffect(() => {
+  //   fetch('https://instalura-api.vercel.app/api/content/faq')
+  //     .then((respostaDoServer) => respostaDoServer.json())
+  //     .then((respostaConvertida) => respostaConvertida.data)
+  //     .then((resposta) => {
+  //       setFaqCategories(resposta);
+  //     });
+  // }, []);
 
   return (
     <FAQScreen faqCategories={faqCategories} />
   );
+}
+
+export async function getStaticProps() {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then((respostaDoServer) => respostaDoServer.json())
+    .then((respostaConvertida) => respostaConvertida.data);
+
+  return {
+    props: {
+      faqCategories,
+    },
+  };
 }
