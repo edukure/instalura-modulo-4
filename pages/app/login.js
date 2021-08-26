16afdb34dce5d6c8d76997b934e6a2aa49c8ff7b
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-26 19:23:08 -0300

    implementar websitePageHOC no Login

--- pages/app/login.js
+++ pages/app/login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
-export default function LoginPage() {
+function LoginScreen() {
   return (
     <div>
-      Login
+      Página de Login
     </div>
   );
 }
+
+export default websitePageHOC(LoginScreen, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Login',
+    },
+    menuProps: {
+      display: false,
+    },
+  },
+});
