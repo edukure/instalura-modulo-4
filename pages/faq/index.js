cf01e7b6d88ffb3f08a77be25ba8ec20a4f18a62
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-26 17:31:05 -0300

    pegar dados do servidor com fetch no FAQPage

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
 
 export default function FAQPage() {
+  const [faqCategories, setFaqCategories] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://instalura-api.vercel.app/api/content/faq')
+      .then((respostaDoServer) => respostaDoServer.json())
+      .then((respostaConvertida) => respostaConvertida.data)
+      .then((resposta) => {
+        setFaqCategories(resposta);
+      });
+  }, []);
+
   return (
-    <FAQScreen />
+    <FAQScreen faqCategories={faqCategories} />
   );
 }
