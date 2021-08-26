5694260c84c2c0ba54ffb59d7af1dfd389e8fa7c
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-26 19:18:49 -0300

    implementar websitePageHOC no FAQPage

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
 // eslint-disable-next-line react/prop-types
-export default function FAQPage({ faqCategories }) {
-  // const [faqCategories, setFaqCategories] = React.useState([]);
-
-  // React.useEffect(() => {
-  //   fetch('https://instalura-api.vercel.app/api/content/faq')
-  //     .then((respostaDoServer) => respostaDoServer.json())
-  //     .then((respostaConvertida) => respostaConvertida.data)
-  //     .then((resposta) => {
-  //       setFaqCategories(resposta);
-  //     });
-  // }, []);
-
+function FAQPage({ faqCategories }) {
   return (
     <FAQScreen faqCategories={faqCategories} />
   );
 }
+
+export default websitePageHOC(FAQPage, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Perguntas Frequentes',
+    },
+  },
+});
 
 export async function getStaticProps() {
   const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
