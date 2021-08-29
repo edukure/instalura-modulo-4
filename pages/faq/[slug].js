899f532759232f39de69f2b632d3a6f65628f863
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-29 17:34:32 -0300

    buscar na API a informação da pergunta

--- pages/faq/[slug].js
+++ pages/faq/[slug].js
@@ -11,24 +11,42 @@ const FAQInternaScreen = ({ category, question }) => (
 
 FAQInternaScreen.propTypes = FAQQuestionScreen.propTypes;
 
-export default websitePageHOC(FAQInternaScreen, {
-  pageWrapperProps: {
-    seoProps: {
-      headTitle: 'Pergunta X',
-    },
-  },
-});
+export default websitePageHOC(FAQInternaScreen);
 
 export const getStaticProps = async ({ params }) => {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then(async (respostaDoServer) => {
+      const resposta = await respostaDoServer.json();
+      return resposta.data;
+    });
+
+  const dadosDaPagina = faqCategories.reduce((valorAcumulado, faqCategory) => {
+    const foundQuestion = faqCategory.questions.find((question) => {
+      if (question.slug === params.slug) {
+        return true;
+      }
+      return false;
+    });
+
+    if (foundQuestion) {
+      return {
+        ...valorAcumulado,
+        category: faqCategory,
+        question: foundQuestion,
+      };
+    }
+
+    return valorAcumulado;
+  }, {});
+
   return {
     props: {
-      category: {
-        questions: [],
-      },
-      question: {
-        title: 'titulo',
-        slug: params.slug,
-        description: 'lorem ipsum blabla',
+      category: dadosDaPagina.category,
+      question: dadosDaPagina.question,
+      pageWrapperProps: {
+        seoProps: {
+          headTitle: dadosDaPagina.question.title,
+        },
       },
     },
   };
