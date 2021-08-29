49ac4d9302dd31816b4c5cf19a89c3a7cfb727a3
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-08-29 17:35:04 -0300

    ajustar as props do websitePageWrapper

--- src/components/wrappers/WebsitePage/hoc/index.js
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -1,11 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import WebsitePageWrapper from '..';
 import WebsiteGlobalProvider from '../provider';
 
-const websitePageHOC = (PageComponent, { pageWrapperProps }) => (props) => (
+const websitePageHOC = (
+  PageComponent,
+  { pageWrapperProps } = { pageWrapperProps: {} },
+) => (props) => (
   <WebsiteGlobalProvider>
-    <WebsitePageWrapper {...pageWrapperProps}>
+    <WebsitePageWrapper
+      {...pageWrapperProps}
+      {...props.pageWrapperProps}
+    >
       <PageComponent {...props} />
     </WebsitePageWrapper>
   </WebsiteGlobalProvider>
