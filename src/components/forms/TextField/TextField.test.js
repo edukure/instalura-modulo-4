2d9b283f1c766f1984b6700cc9a224a99e343008
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-09-06 02:01:22 -0300

    criar testUtils

--- src/components/forms/TextField/TextField.test.js
+++ src/components/forms/TextField/TextField.test.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../infra/test/testUtils';
 
 import TextField from './index';
-import WebSiteGlobalProvider from '../../wrappers/WebsitePage/provider';
 
 describe('<TextField />', () => {
   test('renders component', () => {
     render(
-      <WebSiteGlobalProvider>
         <TextField placeholder="Nome" value="ju" onChange={() => {}} name="nome" />
-      </WebSiteGlobalProvider>,
     );
 
     const textField = screen.getByPlaceholderText(/nome/i);
