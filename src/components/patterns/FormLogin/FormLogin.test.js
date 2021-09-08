29ad95547c08357a00b0fe6dd5ecd516f96bddc9
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-09-08 00:06:23 -0300

    criar teste de campo Senha inválido

--- src/components/patterns/FormLogin/FormLogin.test.js
+++ src/components/patterns/FormLogin/FormLogin.test.js
@@ -41,16 +41,30 @@ describe('<FormLogin />', () => {
   });
 
   describe('when form fields are invalid', () => {
-    test('displays the respective errors', async () => {
-      render(<FormLogin onSubmit={onSubmit} />);
+    describe('displays the respective errors', () => {
+      test('invalid Usuário field', async () => {
+        render(<FormLogin onSubmit={onSubmit} />);
 
-      const inputUsuario = screen.getByPlaceholderText('Usuário');
-      inputUsuario.focus();
-      inputUsuario.blur();
+        const inputUsuario = screen.getByPlaceholderText('Usuário');
+        inputUsuario.focus();
+        inputUsuario.blur();
 
-      await waitFor(() => screen.getByRole('alert'));
+        await waitFor(() => screen.getByRole('alert'));
 
-      expect(screen.getByRole('alert')).toHaveTextContent('Preencha ao menos 3 caracteres');
+        expect(screen.getByRole('alert')).toHaveTextContent('Preencha ao menos 3 caracteres');
+      });
+
+      test('invalid Senha field', async () => {
+        render(<FormLogin onSubmit={onSubmit} />);
+
+        const inputSenha = screen.getByPlaceholderText('Senha');
+        inputSenha.focus();
+        inputSenha.blur();
+
+        await waitFor(() => screen.getByRole('alert'));
+
+        expect(screen.getByRole('alert')).toHaveTextContent('Sua senha precisa ter ao menos 8 caracteres');
+      });
     });
   });
 });
